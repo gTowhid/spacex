bd16c4daaec60bfffb9ca4d5043ed3b5d1470703
Towhid <towhidgolam8182@gmail.com>
2023-02-09 23:53:25 +0600

    Sidebar & counter incorporated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
 
 btn.addEventListener('click', navToggle);
 
@@ -7,4 +9,66 @@ function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
   document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
 }
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      // get count target
+      const target = parseInt(counter.getAttribute('data-target'));
+      // get current counter value
+      const c = parseInt(counter.innerText);
+      // create an increment
+      const increment = target / 100;
+      // if counter is less than target, add increment
+      if (c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function countDown() {
+  counters.forEach((counter) => {
+    const updateCounter = () => {
+      // get count target
+      const target = 0;
+      // get current counter value
+      const c = parseInt(counter.innerText);
+      // create an decrement
+      const decrement = c / 100;
+      // if counter is greater than target, substract decrement
+      if (c > target) {
+        counter.innerText = `${Math.floor(c - decrement)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+const options = {
+  rootMargin: '0px',
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(
+  (entries) =>
+    entries.forEach((entry) => {
+      entry.isIntersecting ? countUp() : countDown();
+    }),
+  options
+);
+const point = document.querySelector('.stats');
+observer.observe(point);
